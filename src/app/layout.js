57de45d2a1b3a771949330928480e68c735b5689
Simone <151656886+simone013124@users.noreply.github.com>
2025-03-25 14:37:48 +0100

    debug deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,8 +25,8 @@ export default function RootLayout({ children }) {
               <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
                 <img src="/logo_inverted.png" alt="Logo" className="h-12" />
                 <div className="mr-12">
-                  <a href="/" className="mr-4">Home</a>
-                  <a href="/dashboard">Info</a>
+                  <Link href="/" className="mr-4">Home</Link>
+                  <Link href="/dashboard">Info</Link>
                 </div>
               </nav>
             {children}
